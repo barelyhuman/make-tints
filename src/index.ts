696da76fa0201e6c91ab43f1100d7e9dd33dd35d
Reaper <ahoy@barelyhuman.dev>
2022-04-02 00:41:04 +0530

    add esm support from tocolor

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,31 @@
-import { hexToHSL, hslToHex } from '@barelyhuman/tocolor'
+import { hexToHSL, hslToHex } from "@barelyhuman/tocolor";
 
 function lighter(percentage: number, color: string) {
-  if (isNaN(percentage)) return color
-  const { h, s, l } = hexToHSL(color)
-  let _afterLighten = l + percentage
-  if (_afterLighten > 100) _afterLighten = 100
-  return hslToHex(h, s, _afterLighten)
+  if (isNaN(percentage)) return color;
+  const { h, s, l } = hexToHSL(color);
+  let _afterLighten = l + percentage;
+  if (_afterLighten > 100) _afterLighten = 100;
+  return hslToHex(h, s, _afterLighten);
 }
 
 interface TintColorMap {
-  base: string
-  tones: number[]
+  base: string;
+  tones: number[];
 }
 
 export function makeTints(toTint: TintColorMap[]) {
-  const result = []
+  const result = [];
   for (let i = 0; i < toTint.length; i++) {
-    const toTintItem = toTint[i]
+    const toTintItem = toTint[i];
     const _item: { base: string; [key: number]: string } = {
       base: lighter(0, toTintItem.base),
-    }
+    };
     toTintItem.tones.forEach((tone) => {
-      const per = 100 - tone
-      _item[tone] = lighter(per >= 0 ? per : 0, toTintItem.base)
-    })
+      const per = 100 - tone;
+      _item[tone] = lighter(per >= 0 ? per : 0, toTintItem.base);
+    });
 
-    result.push(_item)
+    result.push(_item);
   }
-  return result
+  return result;
 }
