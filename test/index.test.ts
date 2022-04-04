ecac0ada06aa784d048b0cf63f2436e7b426e515
Reaper <ahoy@barelyhuman.dev>
2022-04-04 17:40:59 +0530

    modify to add in @barelyhuman defaults + tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,18 @@
-import { describe, expect, it } from 'vitest'
+import {describe, expect, it} from 'vitest'
+import {makeTints} from '../src'
 
 describe('should', () => {
-  it('exported', () => {
-    expect(1).toEqual(1)
-  })
+	it('export tones for #000', () => {
+		const expected = [
+			{'20': 'cccccc', '40': '999999', '60': '666666', base: '000000'},
+		]
+		const tones = makeTints([
+			{
+				base: '#000',
+				tones: [20, 40, 60],
+			},
+		])
+
+		expect(tones).toEqual(expected)
+	})
 })
