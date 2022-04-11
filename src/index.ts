4be29877bb5ecc782b49b20988b11300af244ec1
Reaper <ahoy@barelyhuman.dev>
2022-04-12 05:27:56 +0530

    add tint curry func, neg tint handler
    curry function exposed called `tint` for quicker generation
    and fixes negative percentages

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,39 @@
-import {hexToHSL, hslToHex} from '@barelyhuman/tocolor'
-
-function lighter(percentage: number, color: string) {
-	if (isNaN(percentage)) return color
-	const {h, s, l} = hexToHSL(color)
-	let _afterLighten = l + percentage
-	if (_afterLighten > 100) _afterLighten = 100
-	return hslToHex(h, s, _afterLighten)
-}
+import {hexToHSL, hslToHex, parseToHex} from '@barelyhuman/tocolor'
 
 interface TintColorMap {
 	base: string
 	tones: number[]
 }
 
+interface TintColorResult {
+	base: string
+	key: string
+	[key: number]: string
+}
+
+function lighter(percentage: number, color: string) {
+	if (isNaN(percentage)) return color
+	if (percentage < 0) percentage = 0
+	const {h, s, l} = hexToHSL(color)
+	let _afterLighten = l + percentage
+	if (_afterLighten > 100) _afterLighten = 100
+	return hslToHex(h, s, _afterLighten)
+}
+
+export function tint(color: string) {
+	return (percentage: number) => lighter(100 - percentage, parseToHex(color))
+}
+
+/**
+ * @deprecated - please use `tint` to create a curried tinter
+ */
 export function makeTints(toTint: TintColorMap[]) {
 	const result = []
 	for (let i = 0; i < toTint.length; i++) {
 		const toTintItem = toTint[i]
-		const _item: {base: string; [key: number]: string} = {
-			base: lighter(0, toTintItem.base),
+		const _item: TintColorResult = {
+			base: parseToHex(toTintItem.base),
+			key: toTintItem.base,
 		}
 		toTintItem.tones.forEach(tone => {
 			const per = 100 - tone
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,18 +1,32 @@
 import {describe, expect, it} from 'vitest'
-import {makeTints} from '../src'
+import {makeTints, tint} from '../src'
 
 describe('should', () => {
-	it('export tones for #000', () => {
+	it('export tones for #000 | all at once', () => {
+		const keyColor = '#000'
 		const expected = [
-			{'20': 'cccccc', '40': '999999', '60': '666666', base: '000000'},
+			{
+				'20': 'cccccc',
+				'40': '999999',
+				'60': '666666',
+				base: '000000',
+				key: keyColor,
+			},
 		]
 		const tones = makeTints([
 			{
-				base: '#000',
+				base: keyColor,
 				tones: [20, 40, 60],
 			},
 		])
 
 		expect(tones).toEqual(expected)
+	})
+	it('export tones for #000 | curried', () => {
+		const keyColor = '#000'
+		const tinter = tint(keyColor)
+		expect(tinter(20)).toEqual('cccccc')
+		expect(tinter(40)).toEqual('999999')
+		expect(tinter(60)).toEqual('666666')
 	})
 })
